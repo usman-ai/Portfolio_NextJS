b09369d28df10823f79036667e88257f32b7234a
Usman <mohamedusman9644@gmail.com>
2023-05-09 12:22:40 +0530

    Full project

---
+++ src/components/Skills.js
@@ -0,0 +1,49 @@
+import React from "react";
+import { motion } from "framer-motion";
+
+const Skill = ({ name, x, y }) => {
+  return (
+    <motion.div
+      className="flex items-center justify-center rounded-full font-semibold bg-dark text-light
+        py-3 px-6 shadow-dark cursor-pointer absolute"
+      whileHover={{ scale: 1.05 }}
+      initial={{ x: 0, y: 0 }}
+      whileInView={{ x: x, y: y }}
+      transition={{ duration: 1.5 }}
+      viewport={{ once: true }}
+    >
+      {name}
+    </motion.div>
+  );
+};
+
+const Skills = () => {
+  return (
+    <>
+      <h2 className="font-bold text-8xl mt-64 w-full text-center">Skills</h2>
+      <div className="w-full h-screen relative flex items-center justify-center rounded-full bg-circularLight">
+        <motion.div
+          className="flex items-center justify-center rounded-full font-semibold bg-dark text-light
+        p-8 shadow-dark cursor-pointer"
+          whileHover={{ scale: 1.05 }}
+        >
+          Web
+        </motion.div>
+        <Skill name="CSS" x="-23vw" y="1vw" />
+        <Skill name="HTML" x="-25vw" y="-10vw" />
+        <Skill name="Javascript" x="20vw" y="12vw" />
+        <Skill name="ReactJS" x="0vw" y="13vw" />
+        <Skill name="NextJS" x="20vw" y="-18vw" />
+        <Skill name="NodeJS" x="25vw" y="-10vw" />
+        <Skill name="Express" x="15vw" y="1vw" />
+        <Skill name="Figma" x="0vw" y="-21vw" />
+        <Skill name="TailwindCSS" x="-17vw" y="15vw" />
+        <Skill name="Material UI" x="-18vw" y="-19vw" />
+        <Skill name="MongoDB" x="0vw" y="-13vw" />
+        <Skill name="SQL" x="-36vw" y="8vw" />
+      </div>
+    </>
+  );
+};
+
+export default Skills;
