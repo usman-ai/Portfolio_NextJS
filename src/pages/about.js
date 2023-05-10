49e9893589da5f00070deea1e3ad8e3514e00c7d
Usman <mohamedusman9644@gmail.com>
2023-05-10 16:56:56 +0530

    Error Fix Use Effect

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-no-comment-textnodes */
 import AnimatedText from "@/components/AnimatedText";
 import Layout from "@/components/Layout";
 import Head from "next/head";
@@ -29,7 +30,7 @@ const AnimatedNumbers = ({ value }) => {
         ref.current.textContent = latest.toFixed(0);
       }
     });
-  }, []);
+  }, [springValue, value]);
 
   return <span ref={ref}></span>;
 };
@@ -50,19 +51,19 @@ const about = () => {
                 Biography
               </h2>
               <p className="font-medium">
-                Hi, I'm Mohamed Usman, a web developer and UI/UX designer with a
+                Hi, Im Mohamed Usman, a web developer and UI/UX designer with a
                 passion for creating beautiful, functional, and user-centered
                 digital experiences. With 4 years of experience in the field. I
                 am always looking for new and innovative ways to bring my
-                clients' visions to life.
+                clients visions to life.
               </p>
               <p className=" my-4 font-medium">
                 I believe that design is about more than just making things look
-                pretty – it's about solving problems and creating intuitive,
+                pretty – its about solving problems and creating intuitive,
                 enjoyable experiences for users.
               </p>
               <p className="font-medium">
-                Whether I'm working on a website, mobile app, or other digital
+                Whether Im working on a website, mobile app, or other digital
                 product, I bring my commitment to design excellence and
                 user-centered thinking to every project I work on. I look
                 forward to the opportunity to bring my skills and passion to
